b013297671116cd95f7002061589fd426682348a
Linda Tonui <tonuilinda@gmail.com>
2020-07-03 15:10:24 +0300

    Add App.java class and template folder

---
+++ src/main/java/App.java
@@ -0,0 +1,21 @@
+import static spark.Spark.*;
+
+public class App {
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+    public static void main(String[] args) {
+
+        port(getHerokuAssignedPort());
+        staticFileLocation("/public");
+    }
+
+
+    }
+
