37c3894fd025fccec91c7337f64c363b4883faf6
Linda Tonui <tonuilinda@gmail.com>
2020-07-04 16:10:11 +0300

    add model heroes and its tests

--- src/main/java/model/Hero.java
+++ src/main/java/model/Hero.java
@@ -1,7 +1,51 @@
 package model;
 
-public class Hero {
-    public Hero() {
+import java.util.ArrayList;
 
+public class Hero {
+    private String heroName;
+    private int age;
+    private String superPower;
+    private String weakness;
+    private static ArrayList<Hero> heroes= new ArrayList<>();
+    private int id;
+
+    public Hero(String heroName,int age,String superPower,String weakness) {
+
+        this.heroName=heroName;
+        this.age = age;
+        this.superPower= superPower;
+        this.weakness = weakness;
+        heroes.add(this);
+        id= heroes.size();
+
+
+    }
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSuperPower() {
+        return superPower;
+    }
+
+    public String getWeakness() {
+        return weakness;
+    }
+
+    public static ArrayList<Hero> getHeroes() {
+        return heroes;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public static Hero findById(int id){
+        return heroes.get(id-1);
     }
 }
--- src/test/java/model/HeroTest.java
+++ src/test/java/model/HeroTest.java
@@ -1,8 +1,69 @@
 package model;
 
+import org.junit.Test;
+
+import java.awt.print.Pageable;
+
 import static org.junit.Assert.*;
 
 public class HeroTest {
 
+    @Test
+    public void Hero_InstantiatesHeroCorectly_true() {
+        Hero hero = new Hero("Super Man",22,"Fly","Kryptonite");
+        assertTrue(hero instanceof Hero);
+    }
 
+    @Test
+    public void Hero_getsHeroNAme_String() {
+        Hero hero = new Hero("Super Man",22,"Fly","Kryptonite");
+        assertEquals("Super Man",hero.getHeroName());
+    }
+
+    @Test
+    public void Hero_getsHeroAge_Integer() {
+        Hero hero = new Hero("Super Man",22,"Fly","Kryptonite");
+        assertEquals(22,hero.getAge());
+    }
+
+    @Test
+    public void getSuperPower_getsHeroSuperPower_Integer() {
+        Hero hero = new Hero("Super Man",22,"Fly","Kryptonite");
+        assertEquals("Fly",hero.getSuperPower());
+    }
+
+    @Test
+    public void getWeakness_getsHeroWeakness_Integer() {
+        Hero hero = new Hero("Super Man",22,"Fly","Kryptonite");
+        assertEquals("Kryptonite",hero.getWeakness());
+    }
+
+    @Test
+    public void getHeroes_getsAllHeroesInSquad() {
+        Hero hero = new Hero("Super Man",22,"Fly","Kryptonite");
+        assertEquals(1,Hero.getHeroes().size());
+    }
+
+    @Test
+    public void GetsAllHeroesIfHeroesAreMoreThanOne() {
+        Hero hero = new Hero("Super Man",22,"Fly","Kryptonite");
+        Hero hero2 = new Hero("Spider Man", 35,"Spider Sense","Ethyl Chloride pesticide");
+        assertEquals(2,Hero.getHeroes().size());
+    }
+
+    @Test
+    public void getHeroes_ContainsAllInstances_true() {
+        Hero hero = new Hero("Super Man",22,"Fly","Kryptonite");
+        Hero hero2 = new Hero("Spider Man", 35,"Spider Sense","Ethyl Chloride pesticide");
+        assertTrue(Hero.getHeroes().contains(hero));
+        assertTrue(Hero.getHeroes().contains(hero));
+    }
+
+    @Test
+    public void ReturnsCorrectId() {
+        Hero hero = new Hero("Super Man",22,"Fly","Kryptonite");
+        Hero hero2 = new Hero("Spider Man", 35,"Spider Sense","Ethyl Chloride pesticide");
+        assertEquals(1,Hero.findById(hero.getId()).getId());
+        assertEquals(2,Hero.findById(hero2.getId()).getId());
+    }
 }
